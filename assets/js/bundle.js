c2063cf9ae49df0b03d02d57da88a7f651310f14
Kelly J Andrews <kelly@kellyjandrews.com>
2018-04-25 12:36:26 -0400

    remove jquery transitions, closes #6

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -88,16 +88,6 @@ var $ = jQuery = __webpack_require__(2),
 
 $(function(){
 
-	$('article.post').on('mouseover', function(){
-		$(this).find('header h1').addClass('blurry-text');
-		$(this).find('section').removeClass('hidden');
-		//$(this).find('.overlay').removeClass('hidden');
-	}).on('mouseout', function(){
-		$(this).find('header h1').removeClass('blurry-text');
-		$(this).find('section').addClass('hidden');
-		//$(this).find('.overlay').addClass('hidden');
-	});
-
 	if($('#experience').length) {
 
 		//set up templates
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,16 +6,6 @@ var $ = jQuery = require('jquery'),
 
 $(function(){
 
-	$('article.post').on('mouseover', function(){
-		$(this).find('header h1').addClass('blurry-text');
-		$(this).find('section').removeClass('hidden');
-		//$(this).find('.overlay').removeClass('hidden');
-	}).on('mouseout', function(){
-		$(this).find('header h1').removeClass('blurry-text');
-		$(this).find('section').addClass('hidden');
-		//$(this).find('.overlay').addClass('hidden');
-	});
-
 	if($('#experience').length) {
 
 		//set up templates
